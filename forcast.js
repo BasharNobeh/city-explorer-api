66c85aff0e220e8bee61c16d265845abc3fe4bce
BasharNobeh <Basharnobeh2001@gmail.com>
2022-08-30 02:29:44 +0300

    done

---
+++ forcast.js
@@ -0,0 +1,107 @@
+class Forcast {
+  constructor() {
+    const weatherData = require("./Assets/weather.json");
+    this.cityName = weatherData.map((cityName) => cityName.city_name);
+    this.date = weatherData.map((item) =>
+      item.data.map((index) => index.valid_date)
+    );
+    this.description = weatherData.map((item) =>
+      item.data.map((index) => index.weather.description)
+    );
+    this.lat = weatherData.map((lat) => lat.lat);
+    this.lon = weatherData.map((lon) => lon.lon);
+    this.low = weatherData.map((item) =>
+      item.data.map((index) => index.low_temp)
+    );
+    this.high = weatherData.map((item) =>
+      item.data.map((index) => index.max_temp)
+    );
+  }
+
+  Amman() {
+    let date = this.date[2];
+    let description = this.description[2];
+    let low = this.low[2];
+    let high = this.high[2];
+    const reponseData = [
+      {
+        description: `Low of ${low[0]}, high of ${high[0]} with ${description[0]}`,
+        date: date[0],
+      },
+      {
+        description: `Low of ${low[1]}, high of ${high[1]} with ${description[1]}`,
+        date: date[1],
+      },
+      {
+        description: `Low of ${low[2]}, high of ${high[2]} with ${description[2]}`,
+        date: date[2],
+      },
+    ];
+    return reponseData;
+  }
+
+  Seattle() {
+    let date = this.date[0];
+    let description = this.description[0];
+    let low = this.low[0];
+    let high = this.high[0];
+    const reponseData = [
+      {
+        description: `Low of ${low[0]}, high of ${high[0]} with ${description[0]}`,
+        date: date[0],
+      },
+      {
+        description: `Low of ${low[1]}, high of ${high[1]} with ${description[1]}`,
+        date: date[1],
+      },
+      {
+        description: `Low of ${low[2]}, high of ${high[2]} with ${description[2]}`,
+        date: date[2],
+      },
+    ];
+    return reponseData;
+  }
+
+  Paris() {
+    let date = this.date[1];
+    let description = this.description[1];
+    let low = this.low[1];
+    let high = this.high[1];
+    const reponseData = [
+      {
+        description: `Low of ${low[0]}, high of ${high[0]} with ${description[0]}`,
+        date: date[0],
+      },
+      {
+        description: `Low of ${low[1]}, high of ${high[1]} with ${description[1]}`,
+        date: date[1],
+      },
+      {
+        description: `Low of ${low[2]}, high of ${high[2]} with ${description[2]}`,
+        date: date[2],
+      },
+    ];
+    return reponseData;
+  }
+
+  AmmanCoordinates() {
+    return [this.lat[2], this.lon[2]];
+  }
+
+  ParisCoordinates() {
+    return [this.lat[1], this.lon[1]];
+  }
+
+  SeattleCoordinates() {
+    return [this.lat[0], this.lon[0]];
+  }
+}
+
+const data = new Forcast();
+
+module.exports.amman = data.Amman();
+module.exports.paris = data.Paris();
+module.exports.seattle = data.Seattle();
+module.exports.ammancoordinates = data.AmmanCoordinates();
+module.exports.parisCoordinates = data.ParisCoordinates();
+module.exports.seattleCoordinates = data.SeattleCoordinates();
--- server.js
+++ server.js
@@ -1,68 +1,52 @@
-'use strict';
-
-require('dotenv').config();
-const express = require('express'); //import express
-const cors = require('cors'); //import cors
-const WeatherData = require('./Assets/weather.json');
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const server = express();
-server.use(cors()); // make my server opened for any client
 
-const PORT = process.env.MY_PORT ; //take the port from .env file
+server.use(cors());
 
-// http://localhost:3100/
-server.get('/',(req,res)=>{
-    res.send('This is The defulat route . Happy to have you here :)')
-})
+const PORT = process.env.PORT;
 
-// http://localhost:3100/weather?name=cityName&lon=cityLon&lat=cityLat
-server.get('/weather',(req,res) => {
+const Forcast = require("./forcast");
 
+//http:localhost:3100/
+server.get("/", (req, res) => {
+  res.send("Hi from the Default route");
+});
 
-try{
-    const result = WeatherData.find( (item) =>{
-       
-        
-        if(item.city_name === req.query.name)
-        return item;
-    })  
-    let arr = [];
-    arr.push(result.city_name);
-    arr.push(result.lat )
-    arr.push(result.lon)
-    console.log("array",arr);
+//http:localhost:3100/weather?name=cityName&lon=lon&lat=lat
+server.get("/weather", (req, res) => {
+  if (
+    req.query.lat === Forcast.ammancoordinates[0] &&
+    req.query.lon === Forcast.ammancoordinates[1] &&
+    req.query.name.toLowerCase() === "amman"
+  ) {
+    return res.send(Forcast.amman);
+  } else if (
+    req.query.lat === Forcast.parisCoordinates[0] &&
+    req.query.lon === Forcast.parisCoordinates[1] &&
+    req.query.name.toLowerCase() === "paris"
+  ) {
+    return res.send(Forcast.paris);
+  } else if (
+    req.query.lat === Forcast.seattleCoordinates[0] &&
+    req.query.lon === Forcast.seattleCoordinates[1] &&
+    req.query.name.toLowerCase() === "seattle"
+  ) {
+    return res.send(Forcast.seattle);
+  } else {
+    return res.send(
+      "404 Error, Please Enter Amman, Paris or Seattle to get weather info!"
+    );
+  }
+});
 
-    res.send(arr);
-    this.setState({
-          
-        name: CityData[0].display_name,
-        lat: CityData[1].lat,
-        lon: CityData[2].lon,
-        appear:true,
-        
-      });
+server.get("*", (req, res) => {
+  res.send(
+    "ERROR ! Please Enter A valid route . We could not find this page ! "
+  );
+});
 
-
-}catch{
-
-    res.send("Error ! Please Enter One Of the following :Amman,Seattle or Paris. ");
-
-
-}
-
-})
-
-
-
-// http://localhost:3100/getPock?name=bulbasaur
-
-
-// http:localhost:3100/***** */
-server.get('*',(req,res)=>{
-    res.status(404).send('Sorry, page not found');
-})
-
-// to make our server listen on PORT
 server.listen(PORT, () => {
-    console.log(`Hello, I am listening on ${PORT}`);
-})
+  console.log(`Hello, I am listening On ${PORT}`);
+});
